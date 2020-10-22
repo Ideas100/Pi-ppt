55d1f88a8b237a349569fc5cfa12af3c543e21ae
Pravin Raghul <pravinraghul@gmail.com>
2020-10-22 19:20:41 +0530

    Update test-title-slide.py

--- test/test-title-slide.py
+++ test/test-title-slide.py
@@ -21,7 +21,7 @@ Testing title method
 justify left
 """
 frame2.title(title, font='Times', justify='left')
-frame2.subtitle('Subtitle: right', font='Courier', side='right')
+frame2.subtitle('Subtitle: right', font='Courier', align='right')
 
 # Test-3: add_title_slide
 # title: color red4 &justify right
@@ -32,7 +32,7 @@ Testing title method
 justify right
 """
 frame3.title(title, font_color='red4', justify='right')
-frame3.subtitle('Subtitle: left', font_color='grey35', side='left')
+frame3.subtitle('Subtitle: center', font_color='grey35', align='center')
 
 # Test-4: add_title_slide
 # title: adding long title
@@ -49,14 +49,15 @@ frame4.subtitle('Testing subtitle with a long text')
 # title: spliting a long text into two lines
 frame5 = add_title_slide()
 title = """
-Testing title method with a long title splitting into two lines
- """
+Testing title method with a long title 
+splitting into two lines
+"""
 subtitle="""
 Testing subtitle method with long text
 splitting and justifying it 
 """
 frame5.title(title)
-frame5.subtitle(subtitle, side='right', justify='right')
+frame5.subtitle(subtitle, align='right', justify='right')
 
 # Bundle the frames
 app.bundle(frame1, frame2, frame3, frame4, frame5)
